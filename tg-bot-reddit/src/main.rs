8f0a506e7f54c68fea1e1bb455c1f49f9ac3dba7
Francisco Krause <fkrausear@gmail.com>
2023-05-26 00:41:55 -0300

    Show markpu

--- tg-bot-reddit/src/main.rs
+++ tg-bot-reddit/src/main.rs
@@ -1,10 +1,23 @@
-use teloxide::{prelude::*, utils::command::BotCommands};
+use teloxide::{
+    dispatching::DispatcherBuilder,
+    prelude::*,
+    types::{InlineKeyboardButton, InlineKeyboardMarkup},
+    utils::command::BotCommands,
+};
 mod reddit;
+type WebHandler = Endpoint<'static, DependencyMap, String>;
+use anyhow::Result;
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
     log::info!("Starting command bot...");
     let bot = Bot::from_env();
+    // let handler = dptree::entry().branch(Update::filter_message().endpoint(moderation_handler));
+    // Dispatcher::builder(bot, handler)
+    //     .enable_ctrlc_handler()
+    //     .build()
+    //     .dispatch()
+    //     .await;
     Command::repl(bot, answer).await;
 }
 
@@ -14,42 +27,44 @@ async fn main() {
     description = "These commands are supported:"
 )]
 enum Command {
-    #[command(description = "display this text.")]
-    Help,
-    #[command(description = "handle a username.")]
-    Username(String),
-    #[command(description = "handle a username and an age.", parse_with = "split")]
-    UsernameAndAge { username: String, age: u8 },
-    #[command(description = "Debug")]
-    Debug(String),
+    #[command(description = "Print info about you")]
+    Me,
+    #[command(description = "Get the modqueue")]
+    ModQueue,
 }
 
 async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
     match cmd {
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
-        }
-        Command::Username(username) => {
-            bot.send_message(msg.chat.id, format!("Your username is @{username}."))
-                .await?
-        }
-        Command::UsernameAndAge { username, age } => {
-            bot.send_message(
-                msg.chat.id,
-                format!("Your username is @{username} and age is {age}."),
-            )
-            .await?
-        }
-        Command::Debug(_string) => {
+        Command::Me => {
             let mut client = reddit::Client::new();
             if let Ok(about_me) = &client.reddit_request("/api/v1/me").await {
-                println!(
-                    "The about me json: {}",
-                    serde_json::to_string_pretty(about_me).unwrap()
-                );
-                bot.send_message(msg.chat.id, "Fetched succesfully!")
+                bot.send_message(
+                    msg.chat.id,
+                    format!(
+                        "Fetched succesfully! Your user is {}",
+                        about_me.get("name").unwrap()
+                    ),
+                )
+                .await?
+            } else {
+                bot.send_message(msg.chat.id, "Error accessing token".to_string())
                     .await?
+            }
+        }
+        Command::ModQueue => {
+            let mut client = reddit::Client::new();
+            if let Ok(response) = &client
+                .reddit_request("/r/dankgentina/about/modqueue?limit=1")
+                .await
+            {
+                let mod_options = ["Approve", "Remove"]
+                    .map(|string| InlineKeyboardButton::callback(string, string));
+                bot.send_message(
+                    msg.chat.id,
+                    response["data"]["children"][0]["data"]["link_url"].to_string(),
+                )
+                .reply_markup(InlineKeyboardMarkup::new([mod_options]))
+                .await?
             } else {
                 bot.send_message(msg.chat.id, "Error accessing token".to_string())
                     .await?
